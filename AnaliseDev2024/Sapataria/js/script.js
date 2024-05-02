812c8014063d6eb29fa5ebe8dcdf15e751e605f2
Jotave Reis <j.vitorfdosreis@gmail.com>
2024-05-02 17:32:21 -0300

    progresso?

---
+++ AnaliseDev2024/Sapataria/js/script.js
@@ -0,0 +1,12 @@
+/*
+function logar() {
+   var user = document.getElementById("user").value;
+   var password = document.getElementById("password").value;
+
+   if (user == "admin" && password == "admin") {
+     location.href = "../../../Sapataria/pages/main/adm.html";
+   } else {
+     alert("Senha e/ou Usuário errados");
+   }
+ }
+ */
